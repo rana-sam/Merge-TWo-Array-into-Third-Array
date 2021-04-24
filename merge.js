fff571ae11f82c0b146ade196cc25abe25a08fd1
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-24 10:49:57 +0500

    Create merge.js

---
+++ merge.js
@@ -0,0 +1,8 @@
+let num = [1,2,3,4,5,6,7]
+let num2 = [1,19,3,0,5,9,7]
+// concat return new array merge two or more arrays
+
+let merge = num.concat(num2)
+merge = merge.sort();
+
+console.log(merge);
